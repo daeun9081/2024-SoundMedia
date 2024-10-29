5de09dfb3cbd5f1216bf6960d5d6c692ad7b9214
Daeun Kim <shrch6118@naver.com>
2024-10-30 03:23:50 +0900

    draw trianle

--- Girl_with_a_Pearl_Earring/sketch.js
+++ Girl_with_a_Pearl_Earring/sketch.js
@@ -6,4 +6,10 @@ function setup() {
 
 function draw() {
   image(jj, 0, 0, 1280, 720);
+  console.log(mouseX, mouseY);
+  fill(220);
+  noStroke();
+  triangle(572, 344, 617, 334, 567, 314);
+  fill(0);
+  triangle(567, 314, 617, 334, 609, 274);
 }
