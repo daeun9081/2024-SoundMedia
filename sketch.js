14bad79d62a40b41efbd3a6aa041f99159ab1a2c
Daeun Kim <shrch6118@naver.com>
2024-10-30 09:33:06 +0900

    done

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 function setup() {
-  createCanvas(1280, 720);
-  jj = loadImage("jj.png");
+  createCanvas(1280, 720); // 이미지 크기 1280*720으로 불러오기
+  jj = loadImage("jj.png"); 
 
 }
 
 function draw() {
-  image(jj, 0, 0, 1280, 720);
-  console.log(mouseX, mouseY);
+  image(jj, 0, 0, 1280, 720); //진주귀걸이를 한 소녀 이미지 불러오기
+  console.log(mouseX, mouseY); // 마우스 값 출력
   fill(237,208,187);
-  noStroke();
+  noStroke(); // 테두리 없애기
   triangle(572, 344, 618, 336, 567, 314);
   fill(243,214,207);
   triangle(567, 314, 618, 336, 609, 274);
@@ -25,7 +25,7 @@ function draw() {
   vertex(708, 336);
   vertex(673, 309);
   vertex(653, 227);
-  vertex(696, 271); //653, 227
+  vertex(696, 271); 
   endShape(CLOSE);
   fill(149,174,199);
   triangle(673, 309, 653, 227, 609, 274);
@@ -34,19 +34,19 @@ function draw() {
   fill(52,67,92);
   triangle(708, 336, 785, 328, 696, 271);
   fill(41,37,47);
-  triangle(708, 336, 785, 328, 753, 378); //785, 328
+  triangle(708, 336, 785, 328, 753, 378); 
   fill(164,182,204);
-  triangle(591, 208, 653, 227, 675, 163); //675. 163
+  triangle(591, 208, 653, 227, 675, 163); 
   fill(174,198,227);
   triangle(591, 208, 675, 163, 582, 149);
   fill(43,78,126);
-  triangle(785, 328, 755, 270, 696, 271); //755, 270
+  triangle(785, 328, 755, 270, 696, 271); 
   fill(45,59,82);
   triangle(785, 328, 818, 265, 755, 270);
   fill(44,59,81);
-  triangle(785, 328, 818, 265, 848, 320); //818, 265
+  triangle(785, 328, 818, 265, 848, 320); 
   fill(105,140,179);
-  triangle(818, 265, 826, 213, 756, 229 ); //756, 229
+  triangle(818, 265, 826, 213, 756, 229 );
   fill(91,125,169);
   triangle(696, 271, 755, 270, 653, 227);
   fill(99,132,170);
